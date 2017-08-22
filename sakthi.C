2b7cb0c44690d0ab8f365cfa2f3f6d741eee6ae5
sakthisanjay96 <30748370+sakthisanjay96@users.noreply.github.com>
2017-08-22 15:04:48 +0530

    sakthi.c
    eeee

---
+++ sakthi.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    double num;
+    printf("Enter a num: ");
+    scanf("%lf", &num);
+    if (num <= 0.0)
+    {
+        if (num == 0.0)
+            printf("You entered 0.");
+        else
+            printf("You entered a negative num.");
+    }
+    else
+        printf("You entered a positive num.");
+    return 0;
+}
